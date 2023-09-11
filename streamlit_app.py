11ffec13d3e3aee9d7b7d20d5e67ea37b977989a
morispolanco <morispolanco@gmail.com>
2023-09-11 07:01:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit as st
+import sounddevice as sd
 import speech_recognition as sr
 
-def transcribir_audio(archivo):
+def transcribir_audio(audio):
     r = sr.Recognizer()
     
-    with sr.AudioFile(archivo) as source:
+    with sr.AudioFile(audio) as source:
         audio = r.record(source)
         
     try:
@@ -18,12 +19,25 @@ def transcribir_audio(archivo):
 def main():
     st.title("Transcriptor de mensajes de voz")
     st.write("¡Bienvenido al Transcriptor de mensajes de voz!")
-    st.write("Esta aplicación te permite transcribir mensajes de voz en texto.")
+    st.write("Esta aplicación te permite grabar mensajes de voz y transcribirlos en texto.")
     
-    archivo = st.file_uploader("Selecciona un archivo de audio", type=["wav", "mp3"])
+    grabar = st.button("Grabar")
     
-    if archivo is not None:
-        texto_transcrito = transcribir_audio(archivo)
+    if grabar:
+        # Configurar la grabación de audio
+        fs = 44100  # Frecuencia de muestreo
+        duracion = 5  # Duración de la grabación en segundos
+        
+        # Grabar audio
+        audio = sd.rec(int(duracion * fs), samplerate=fs, channels=1)
+        sd.wait()  # Esperar a que termine la grabación
+        
+        # Guardar el audio en un archivo WAV
+        archivo_wav = "audio.wav"
+        sd.write(archivo_wav, audio, fs)
+        
+        # Transcribir el audio
+        texto_transcrito = transcribir_audio(archivo_wav)
         
         st.success("Transcripción completada:")
         st.write(texto_transcrito)
